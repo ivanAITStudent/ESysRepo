3ca8ea1fdc839bd749983e901bca397f8c605ca8
ivanAITStudent <5726@ait.nsw.edu.au>
2017-05-24 16:02:21 +1000

    bkup

--- A01/Ivan Olea Enterprise Systems Assessment 01 Application/AITLibrary/HomeForm.Designer.cs
+++ A01/Ivan Olea Enterprise Systems Assessment 01 Application/AITLibrary/HomeForm.Designer.cs
@@ -40,9 +40,14 @@
             this.reserve_btn = new System.Windows.Forms.Button();
             this.return_button = new System.Windows.Forms.Button();
             this.details_btn = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -140,7 +145,7 @@
             // 
             // return_button
             // 
-            this.return_button.Location = new System.Drawing.Point(742, 478);
+            this.return_button.Location = new System.Drawing.Point(928, 61);
             this.return_button.Name = "return_button";
             this.return_button.Size = new System.Drawing.Size(146, 23);
             this.return_button.TabIndex = 19;
@@ -156,19 +161,10 @@
             this.details_btn.Text = "DETAILS";
             this.details_btn.UseVisualStyleBackColor = true;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(928, 97);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 23);
-            this.button1.TabIndex = 21;
-            this.button1.Text = "RETURN";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(962, 31);
+            this.label1.Location = new System.Drawing.Point(966, 31);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 13);
             this.label1.TabIndex = 22;
@@ -176,21 +172,81 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(928, 68);
+            this.button2.Location = new System.Drawing.Point(40, 449);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(146, 23);
             this.button2.TabIndex = 23;
-            this.button2.Text = "ACCOUNT";
+            this.button2.Text = "LOGOUT";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(928, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 23);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "REGISTER USER";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(928, 169);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(146, 23);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "ADD NEW MEDIA";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1127, 169);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(146, 23);
+            this.button5.TabIndex = 26;
+            this.button5.Text = "DELETE MEDIA";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1127, 113);
+            this.button6.Name = "button6";
+            this.button6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button6.Size = new System.Drawing.Size(146, 23);
+            this.button6.TabIndex = 27;
+            this.button6.Text = "DELETE USER";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1175, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "ADMIN";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 420);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 23);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "ACCOUNT";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // HomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1285, 552);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.details_btn);
             this.Controls.Add(this.return_button);
             this.Controls.Add(this.reserve_btn);
@@ -226,8 +282,13 @@
         private System.Windows.Forms.Button reserve_btn;
         private System.Windows.Forms.Button return_button;
         private System.Windows.Forms.Button details_btn;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- A01/Ivan Olea Enterprise Systems Assessment 01 Application/AITLibrary/LoginForm.cs
+++ A01/Ivan Olea Enterprise Systems Assessment 01 Application/AITLibrary/LoginForm.cs
@@ -30,27 +30,30 @@ namespace AITLibrary
             //      reset un and pw  
             // else attempt to login
 
+            //LOGIN ATTEMPT
+            try
+            {
                 // get user details
-                    List<TabUserModel> _userList = uL.GetLogin(un, pw);
+                List<TabUserModel> _userList = uL.GetLogin(un, pw);
 
-                // is user exists store details in persistent data
+                // if user exists store details in persistent data
                 // then check userlevel and start a new thread with correct user level form
                 // close this thread
                 if (_userList.Count() > 0)
                 {
                     // Show login success message
-                        message_lbl.Text = "Login Success";
-                    
+                    message_lbl.Text = "Login Success";
+
                     //capture details in persistent data
-                        pUserID = _userList.ElementAt(0).UserID;
-                        pUserName = _userList.ElementAt(0).UserName;
-                        pUserEmail = _userList.ElementAt(0).UserEmail;
-                        pUserLevel = _userList.ElementAt(0).UserLevel;
+                    pUserID = _userList.ElementAt(0).UserID;
+                    pUserName = _userList.ElementAt(0).UserName;
+                    pUserEmail = _userList.ElementAt(0).UserEmail;
+                    pUserLevel = _userList.ElementAt(0).UserLevel;
 
                     //start a new thread with correct user level form
 
-                        System.Threading.Thread t1 = new System.Threading.Thread(new System.Threading.ThreadStart(OpenApplication));
-                        t1.Start();
+                    System.Threading.Thread t1 = new System.Threading.Thread(new System.Threading.ThreadStart(OpenApplication));
+                    t1.Start();
 
                     // destroy old thread
                     this.Close();
@@ -58,12 +61,19 @@ namespace AITLibrary
                 else
                 {
                     // show login failed message
-                        message_lbl.Text = "Login Failed. Please try again";
+                    message_lbl.Text = "Login Failed. Please try again";
                     // clear content of text boxes 
-                        username_tb.Clear();
-                        password_tb.Clear();
+                    username_tb.Clear();
+                    password_tb.Clear();
                 } //endif
 
+            }
+            catch (Exception ex)
+            {
+                // connection failed exception
+                // show login failed message
+                message_lbl.Text = "Connection Failed. Please try again later.";
+            }
         }
 
         private void OpenApplication ()
